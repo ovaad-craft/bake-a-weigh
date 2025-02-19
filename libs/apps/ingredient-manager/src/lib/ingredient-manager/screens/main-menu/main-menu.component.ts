9fb159f099c8cea4bc39f2b6d1b503fe13d4b039
marcus <optiqvision@gmail.com>
2025-02-18 22:26:57 -0600

    generate main menu component for ingredient-manager

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/screens/main-menu/main-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-im-main-menu',
+  imports: [CommonModule],
+  templateUrl: './main-menu.component.html',
+  styleUrl: './main-menu.component.css',
+})
+export class MainMenuComponent {}
