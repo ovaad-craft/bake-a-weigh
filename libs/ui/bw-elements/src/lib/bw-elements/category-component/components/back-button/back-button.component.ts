354797e5baad9ed7ceefa78fdc8884298bae290d
marcus <optiqvision@gmail.com>
2025-02-23 23:18:59 -0600

    generate back button for category component

---
+++ libs/ui/bw-elements/src/lib/bw-elements/category-component/components/back-button/back-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-back-button',
+  imports: [CommonModule],
+  templateUrl: './back-button.component.html',
+  styleUrl: './back-button.component.css',
+})
+export class BackButtonComponent {}
