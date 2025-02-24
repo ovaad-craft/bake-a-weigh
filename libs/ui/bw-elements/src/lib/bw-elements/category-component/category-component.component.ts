59463bdc9d2129b8782b1394a9a2c580fb3294ed
marcus <optiqvision@gmail.com>
2025-02-23 22:10:07 -0600

    generate and expose category component

---
+++ libs/ui/bw-elements/src/lib/bw-elements/category-component/category-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-category-component',
+  imports: [CommonModule],
+  templateUrl: './category-component.component.html',
+  styleUrl: './category-component.component.css',
+})
+export class CategoryComponentComponent {}
