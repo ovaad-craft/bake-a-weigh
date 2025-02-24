a025393ecedf2ba053e0b273f8a72b2b6b59f026
marcus <optiqvision@gmail.com>
2025-02-23 23:20:15 -0600

    generate category item coponent for category coponent

---
+++ libs/ui/bw-elements/src/lib/bw-elements/category-component/components/category-item/category-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-category-item',
+  imports: [CommonModule],
+  templateUrl: './category-item.component.html',
+  styleUrl: './category-item.component.css',
+})
+export class CategoryItemComponent {}
