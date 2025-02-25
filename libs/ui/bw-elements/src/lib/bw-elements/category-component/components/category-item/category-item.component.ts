5d8b05194dcde8534ed91bc4d586912e18d0626e
marcus <optiqvision@gmail.com>
2025-02-24 20:25:13 -0600

    begin specs on category item componet

--- libs/ui/bw-elements/src/lib/bw-elements/category-component/components/category-item/category-item.component.ts
+++ libs/ui/bw-elements/src/lib/bw-elements/category-component/components/category-item/category-item.component.ts
@@ -1,3 +1,53 @@
+/*
+
+Subcategory Button
+
+
+PURPOSE :
+ > Represent an individual item the user can select.
+
+
+ELEMENTS :
+ > img / for cover photo (optional)
+ > svg / for icon (optional)
+ > h5  / for item name
+ 
+
+
+VIEWS : none
+
+
+INTERACTIVITY : none
+
+
+FUNCTIONALITY : none
+
+
+SENDS DATA TO : nowhere
+
+
+GETS DATA FROM :
+ > Category Component
+
+
+ROUTES TO : nowhere
+
+
+USER STORY :
+ > User should see the cover photo if one is provided.
+ > User should see the icon if one is provided.
+ > User should see the name of the item.
+
+
+
+*/
+
+
+
+
+
+
+
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryIndexItem } from '@bake-a-weigh/site-types';
