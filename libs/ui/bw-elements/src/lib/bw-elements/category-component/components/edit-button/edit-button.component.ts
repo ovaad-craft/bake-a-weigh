aeb6564d715bac9949c4a6280809819a119614b7
marcus <optiqvision@gmail.com>
2025-02-24 20:49:19 -0600

    markup and begin specs on edit button component

--- libs/ui/bw-elements/src/lib/bw-elements/category-component/components/edit-button/edit-button.component.ts
+++ libs/ui/bw-elements/src/lib/bw-elements/category-component/components/edit-button/edit-button.component.ts
@@ -1,3 +1,48 @@
+/*
+
+Edit Button
+
+
+PURPOSE :
+ > Aid User in navigating to where changes can be made to indicative item.
+
+
+ELEMENTS :
+ > svg / for icon
+ 
+
+
+VIEWS : none
+
+
+INTERACTIVITY : none
+
+
+FUNCTIONALITY : none
+
+
+SENDS DATA TO : nowhere
+
+
+GETS DATA FROM : nowhere
+
+
+ROUTES TO : nowhere
+
+
+USER STORY :
+ > User should see an icon indicating this provides them witht the ability to make changes.
+
+
+
+*/
+
+
+
+
+
+
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
