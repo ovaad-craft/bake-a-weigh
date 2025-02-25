5575feacfd360ac790bbea4b26c156ee31d32f8b
marcus <optiqvision@gmail.com>
2025-02-24 20:03:26 -0600

    start specs for add button component

--- libs/ui/bw-elements/src/lib/bw-elements/category-component/components/add-button/add-button.component.ts
+++ libs/ui/bw-elements/src/lib/bw-elements/category-component/components/add-button/add-button.component.ts
@@ -1,3 +1,50 @@
+/*
+
+Add Button
+
+
+PURPOSE :
+ > Serve as an indicator that the user would like to add an item to the respective category. 
+
+
+ELEMENTS :
+ > container for plus sign
+ > p / for displaying the noun to use for item.
+
+
+VIEWS : none
+
+
+INTERACTIVITY : none
+
+
+FUNCTIONALITY : none
+
+
+SENDS DATA TO : nowhere
+
+
+GETS DATA FROM :
+ > Category Component
+
+
+ROUTES TO : nowhere
+
+
+USER STORY :
+ > User should see a plus sign indicating that this is for adding an item.
+ > User should see text that reads "add (item noun)".
+
+
+
+*/
+
+
+
+
+
+
+
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
