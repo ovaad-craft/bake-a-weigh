e37f36ea3991ea5bf4397f4cd9a8ce65ac1d812d
marcus <optiqvision@gmail.com>
2025-03-02 21:08:31 -0600

    create method for generating form controls of type CreamState

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/views/ingredient-editor/form-generator/primatives/dairy/cream-state-control/cream-state-control.ts
@@ -0,0 +1,16 @@
+import { FormControl } from "@angular/forms";
+import { CreamState }  from "@bake-a-weigh/site-types";
+
+
+
+
+
+export function createCreamStateControl( defaultCreamState : CreamState ) : FormControl< CreamState | null > {
+
+
+
+    return new FormControl< CreamState >( defaultCreamState ? defaultCreamState : 'liquid' );
+
+
+
+}
