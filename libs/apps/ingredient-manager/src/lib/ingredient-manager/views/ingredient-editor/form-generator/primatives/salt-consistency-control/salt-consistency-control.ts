75e8f501e425ae7eb195a9d641d09f7fd917e859
marcus <optiqvision@gmail.com>
2025-03-02 20:21:35 -0600

    create method for generating a form control of type SaltConsistency

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/views/ingredient-editor/form-generator/primatives/salt-consistency-control/salt-consistency-control.ts
@@ -0,0 +1,17 @@
+import { FormControl } from "@angular/forms";
+import { SaltConsistency } from "@bake-a-weigh/site-types";
+
+
+
+
+
+export function createSaltConsistencyControl( defaultConsistency? : SaltConsistency ) : FormControl< SaltConsistency | null > {
+
+
+
+    return new FormControl< SaltConsistency >( defaultConsistency ? defaultConsistency : 'fine' );
+
+
+
+    
+}
