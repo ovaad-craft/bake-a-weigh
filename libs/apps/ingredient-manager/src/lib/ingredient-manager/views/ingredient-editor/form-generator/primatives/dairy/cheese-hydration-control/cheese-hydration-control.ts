c6b4672ca95a6403af3027ee4cff07acebabf621
marcus <optiqvision@gmail.com>
2025-03-02 21:19:44 -0600

    create method for generating form controls of type CheeseHydrationType

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/views/ingredient-editor/form-generator/primatives/dairy/cheese-hydration-control/cheese-hydration-control.ts
@@ -0,0 +1,16 @@
+import { FormControl } from "@angular/forms";
+import { CheeseHydrationType } from "@bake-a-weigh/site-types";
+
+
+
+
+
+export function createCheeseHydrationControl( defaultHydration? : CheeseHydrationType ) : FormControl< CheeseHydrationType | null > {
+
+
+
+    return new FormControl< CheeseHydrationType >( defaultHydration ? defaultHydration : 'dry' );
+
+
+    
+}
