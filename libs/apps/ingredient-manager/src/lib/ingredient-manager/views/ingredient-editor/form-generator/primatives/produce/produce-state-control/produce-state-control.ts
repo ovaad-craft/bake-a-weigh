ff64b427dda63c5323395af19ac704b15de3d69c
marcus <optiqvision@gmail.com>
2025-03-02 21:41:57 -0600

    create method for generating form controls of type ProduceState

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/views/ingredient-editor/form-generator/primatives/produce/produce-state-control/produce-state-control.ts
@@ -0,0 +1,16 @@
+import { FormControl } from "@angular/forms";
+import { ProduceState } from "@bake-a-weigh/site-types";
+
+
+
+
+
+export function createProdueStateControl( defaultProduceState? : ProduceState ) : FormControl< ProduceState | null > {
+
+
+
+    return new FormControl< ProduceState >( defaultProduceState ? defaultProduceState : 'fresh' );
+
+
+
+}
