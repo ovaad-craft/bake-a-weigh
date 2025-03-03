4572a5cf8b393c1a250f0c27be804ec33cc626b5
marcus <optiqvision@gmail.com>
2025-03-02 19:54:29 -0600

    create method for creating form control of type WeightType

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/views/ingredient-editor/form-generator/primatives/weight-control/weight-control.ts
@@ -0,0 +1,8 @@
+import { FormControl } from "@angular/forms";
+import { WeightType } from "@bake-a-weigh/site-types";
+
+
+export function createWeightControl( defaultWeight? : WeightType ) : FormControl<WeightType | null> {
+
+    return new FormControl<WeightType>(defaultWeight ? defaultWeight : 'g');
+}
