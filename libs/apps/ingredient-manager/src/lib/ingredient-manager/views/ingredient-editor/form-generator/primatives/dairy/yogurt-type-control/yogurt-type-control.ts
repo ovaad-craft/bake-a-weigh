8f4af084066bfe62475d754e52329c6eb9f35da9
marcus <optiqvision@gmail.com>
2025-03-02 21:11:35 -0600

    create method for generating form controls of type YogurtType

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/views/ingredient-editor/form-generator/primatives/dairy/yogurt-type-control/yogurt-type-control.ts
@@ -0,0 +1,16 @@
+import { FormControl } from "@angular/forms";
+import { YogurtType }  from "@bake-a-weigh/site-types";
+
+
+
+
+
+export function createYogurtTypeControl( defaultYogurtType? : YogurtType ) : FormControl< YogurtType | null > {
+
+
+
+    return new FormControl< YogurtType >( defaultYogurtType ? defaultYogurtType : 'plain' );
+
+
+
+}
