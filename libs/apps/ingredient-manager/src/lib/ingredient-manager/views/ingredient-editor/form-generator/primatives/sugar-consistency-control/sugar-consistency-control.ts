68803f43d0f20e9342498f0c67e988575b10f615
marcus <optiqvision@gmail.com>
2025-03-02 20:41:09 -0600

    create method for generating form controls of type SugarConsistencyType

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/views/ingredient-editor/form-generator/primatives/sugar-consistency-control/sugar-consistency-control.ts
@@ -0,0 +1,17 @@
+import { FormControl }          from "@angular/forms";
+import { SugarConsistencyType } from "@bake-a-weigh/site-types";
+
+
+
+
+
+export function createSugarConsistencyControl( defaultConsistency? : SugarConsistencyType ) : FormControl< SugarConsistencyType | null > {
+
+
+
+    return new FormControl< SugarConsistencyType >( defaultConsistency ? defaultConsistency : 'small granules' );
+
+
+
+
+}
