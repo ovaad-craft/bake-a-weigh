c4bc4c58d238320c5342801a0fcd266f7a94d6e8
marcus <optiqvision@gmail.com>
2025-03-02 21:47:04 -0600

    create method for generating form controls of type PlantPart

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/views/ingredient-editor/form-generator/primatives/plant/plant-part-control/plant-part-control.ts
@@ -0,0 +1,16 @@
+import { FormControl } from "@angular/forms";
+import { PlantPart }   from "@bake-a-weigh/site-types";
+
+
+
+
+
+export function createPlantPartControl( defaultPart? : PlantPart ) : FormControl< PlantPart | null > {
+
+
+
+    return new FormControl< PlantPart >( defaultPart ? defaultPart : 'leaf' );
+
+
+
+}
