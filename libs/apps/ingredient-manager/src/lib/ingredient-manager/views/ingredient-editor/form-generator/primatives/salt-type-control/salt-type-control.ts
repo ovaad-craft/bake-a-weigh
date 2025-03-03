e0175c39e7510584cf898dd725cc3b8490079c0b
marcus <optiqvision@gmail.com>
2025-03-02 20:15:55 -0600

    create method for generating a form control of type SaltType

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/views/ingredient-editor/form-generator/primatives/salt-type-control/salt-type-control.ts
@@ -0,0 +1,16 @@
+import { FormControl } from "@angular/forms";
+import { SaltType }    from "@bake-a-weigh/site-types";
+
+
+
+
+
+export function createSaltTypeControl( defaultSaltType? : SaltType ) : FormControl< SaltType | null > {
+
+
+
+    return new FormControl< SaltType >( defaultSaltType ? defaultSaltType : 'table salt' );
+
+
+
+}
