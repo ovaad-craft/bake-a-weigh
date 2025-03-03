9af71d4fa15993d10c544adc6a5cbb7499577bd2
marcus <optiqvision@gmail.com>
2025-03-03 01:23:09 -0600

    create method for generating form groups of type NutreintTrackerGroup

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/views/ingredient-editor/form-generator/primatives/multi-value/nutrient-tracker-group/nutrient-tracker-group.ts
@@ -0,0 +1,24 @@
+import { FormControl, FormGroup }   from "@angular/forms";
+import { NutrientTracker }          from "@bake-a-weigh/site-types";
+import { NutrientTrackerGroup }     from "../../../form-types";
+import { createElementWeightGroup } from "../element-weight-group/element-weight-group";
+
+
+
+
+
+export function createNutrientGroup( defaultSettings? : NutrientTracker ) : FormGroup< NutrientTrackerGroup > {
+
+
+
+    return new FormGroup< NutrientTrackerGroup >({
+
+        name        : new FormControl< string | null >( defaultSettings ? defaultSettings.name : null ),
+        servingSize : createElementWeightGroup( defaultSettings ? defaultSettings.servingSize : undefined ),
+        amount      : createElementWeightGroup( defaultSettings ? defaultSettings.amount : undefined )
+        
+    });
+
+
+
+}
