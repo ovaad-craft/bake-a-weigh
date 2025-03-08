18ef989ab2363f01f7c5d18b3391b17768318669
marcus <optiqvision@gmail.com>
2025-03-07 21:27:45 -0600

    setup ControlValueAccessor on text input

--- libs/ui/form-controls/elements/primative-inputs/text-input/text-input.component.ts
+++ libs/ui/form-controls/elements/primative-inputs/text-input/text-input.component.ts
@@ -18,11 +18,52 @@ USER STORIES :
 
 */
 
-import { Component } from '@angular/core';
+
+
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'lib-text-input',
   templateUrl: './text-input.component.html',
   styleUrls: ['./text-input.component.css'],
+  providers: [
+    {
+      provide     : NG_VALUE_ACCESSOR,
+      useExisting : forwardRef( () => TextInputComponent ),
+      multi       : true
+    }
+  ]
 })
-export class TextInputComponent {}
+export class TextInputComponent implements OnInit, ControlValueAccessor {
+
+
+
+  @Input() label!: string;
+  @Input() InputSize = '';
+  //SizeClass: string = 'smallInput';
+  
+  field  = '';
+
+  ngOnInit():void{ this.setInputSize(); }
+
+  onChange : any = () =>{/* */};
+  onTouch : any = () => {/* */};
+
+  set value(val : string){
+    this.field = val;
+    this.onChange(val);
+    this.onTouch(val);
+  }
+
+  writeValue(value: string){ this.value = value; }
+
+  registerOnChange(fn: any){ this.onChange = fn; }
+
+  registerOnTouched(onTouched: Function){ this.onTouch = onTouched; }
+
+  setInputSize() : void {/* */}
+
+
+
+}
