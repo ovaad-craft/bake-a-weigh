38fe6a7a9187cb197250eec0d191711c283d7490
marcus <optiqvision@gmail.com>
2025-03-07 20:59:25 -0600

    generate text input component

---
+++ libs/ui/form-controls/elements/primative-inputs/text-input/text-input.component.ts
@@ -0,0 +1,28 @@
+/*
+
+TextInput
+
+PURPOSE :
+
+VIEWS :
+
+ELEMENTS :
+
+INTERACTIVITY :
+
+GETS DATA FROM :
+
+SENDS DATA TO :
+
+USER STORIES :
+
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.css'],
+})
+export class TextInputComponent {}
