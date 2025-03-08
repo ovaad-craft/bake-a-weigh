a325996a6806c75a5a8c4ebd7f1498758e8e349f
marcus <optiqvision@gmail.com>
2025-03-08 01:56:54 -0600

    create inputs for inferring type and inheriting a form control

--- libs/ui/form-controls/elements/primative-inputs/select-input/select-input.component.ts
+++ libs/ui/form-controls/elements/primative-inputs/select-input/select-input.component.ts
@@ -18,11 +18,19 @@ USER STORIES :
 
 */
 
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-select-input',
+  imports : [ ReactiveFormsModule ],
   templateUrl: './select-input.component.html',
   styleUrls: ['./select-input.component.css'],
 })
-export class SelectInputComponent {}
+export class SelectInputComponent<ValueType> {
+
+  @Input() OptionType! : ValueType;
+  @Input() OptionList! : ValueType[];
+  @Input() Control!    : FormControl< ValueType | null>;
+  
+}
