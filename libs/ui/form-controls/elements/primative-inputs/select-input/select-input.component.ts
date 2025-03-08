ecdc6b9958dba982a6ea3d2a043f507fd81a1920
marcus <optiqvision@gmail.com>
2025-03-08 01:42:16 -0600

    generate select input

---
+++ libs/ui/form-controls/elements/primative-inputs/select-input/select-input.component.ts
@@ -0,0 +1,28 @@
+/*
+
+SelectInput
+
+PURPOSE :
+
+VIEWS :
+
+ELEMENTS :
+
+INTERACTIVITY :
+
+GETS DATA FROM :
+
+SENDS DATA TO :
+
+USER STORIES :
+
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-select-input',
+  templateUrl: './select-input.component.html',
+  styleUrls: ['./select-input.component.css'],
+})
+export class SelectInputComponent {}
