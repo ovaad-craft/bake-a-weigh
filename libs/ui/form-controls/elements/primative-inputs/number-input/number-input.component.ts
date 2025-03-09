5d2376332e8f8ec615eb2644da8786c52dceb509
marcus <optiqvision@gmail.com>
2025-03-08 18:14:09 -0600

    generate number input

---
+++ libs/ui/form-controls/elements/primative-inputs/number-input/number-input.component.ts
@@ -0,0 +1,30 @@
+/*
+
+NumberInput
+
+PURPOSE :
+
+VIEWS :
+
+ELEMENTS :
+
+INTERACTIVITY :
+
+GETS DATA FROM :
+
+SENDS DATA TO :
+
+USER STORIES :
+
+*/
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-number-input',
+  imports: [CommonModule],
+  templateUrl: './number-input.component.html',
+  styleUrls: ['./number-input.component.css'],
+})
+export class NumberInputComponent {}
