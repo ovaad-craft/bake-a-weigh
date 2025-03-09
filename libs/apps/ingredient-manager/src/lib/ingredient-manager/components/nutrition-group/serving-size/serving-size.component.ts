57a655a047801442a6a33ff4f3697efc4c6c922f
marcus <optiqvision@gmail.com>
2025-03-08 23:22:28 -0600

    debug configuration so far

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/serving-size/serving-size.component.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/serving-size/serving-size.component.ts
@@ -22,16 +22,25 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ElementWeightGroupType } from '../../../views/ingredient-editor/form-generator/form-types';
-import { NumberInputComponent } from '@form-controls';
+import { NumberInputComponent, SelectInputComponent } from '@form-controls';
+import { WeightType } from '@bake-a-weigh/site-types';
 
 @Component({
   selector: 'lib-serving-size',
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, NumberInputComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NumberInputComponent,
+    SelectInputComponent
+  ],
   templateUrl: './serving-size.component.html',
   styleUrls: ['./serving-size.component.css'],
 })
 export class ServingSizeComponent {
 
   @Input()Control! : FormGroup< ElementWeightGroupType >;
+
+  WeightOptions : WeightType[] = ['g','mcg','mg','ml'];
 
 }
