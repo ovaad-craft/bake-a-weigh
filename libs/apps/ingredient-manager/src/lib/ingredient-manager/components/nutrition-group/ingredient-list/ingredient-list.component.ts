39be469525cadb95149a64773d9cbd15cb536007
marcus <optiqvision@gmail.com>
2025-03-08 19:29:34 -0600

    generate ingredient list for nutrition group

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/ingredient-list/ingredient-list.component.ts
@@ -0,0 +1,30 @@
+/*
+
+IngredientList
+
+PURPOSE :
+
+VIEWS :
+
+ELEMENTS :
+
+INTERACTIVITY :
+
+GETS DATA FROM :
+
+SENDS DATA TO :
+
+USER STORIES :
+
+*/
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-ingredient-list',
+  imports: [CommonModule],
+  templateUrl: './ingredient-list.component.html',
+  styleUrls: ['./ingredient-list.component.css'],
+})
+export class IngredientListComponent {}
