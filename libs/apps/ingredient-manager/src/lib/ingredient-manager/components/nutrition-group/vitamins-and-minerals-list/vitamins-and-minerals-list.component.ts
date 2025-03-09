89beae935672c3f4083b6eb40469baa69ad53933
marcus <optiqvision@gmail.com>
2025-03-09 13:53:58 -0500

    generate vitamins and minerals list component

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/vitamins-and-minerals-list/vitamins-and-minerals-list.component.ts
@@ -0,0 +1,30 @@
+/*
+
+VitaminsAndMineralsList
+
+PURPOSE :
+
+VIEWS :
+
+ELEMENTS :
+
+INTERACTIVITY :
+
+GETS DATA FROM :
+
+SENDS DATA TO :
+
+USER STORIES :
+
+*/
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-vitamins-and-minerals-list',
+  imports: [CommonModule],
+  templateUrl: './vitamins-and-minerals-list.component.html',
+  styleUrls: ['./vitamins-and-minerals-list.component.css'],
+})
+export class VitaminsAndMineralsListComponent {}
