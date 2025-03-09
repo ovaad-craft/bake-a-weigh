f84686e6c64825c81ad3f7ffbce0f09c73888081
marcus <optiqvision@gmail.com>
2025-03-09 14:11:45 -0500

    configure vitamin...component to itereate, add and remove list items

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/vitamins-and-minerals-list/vitamins-and-minerals-list.component.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/vitamins-and-minerals-list/vitamins-and-minerals-list.component.ts
@@ -18,13 +18,38 @@ USER STORIES :
 
 */
 
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormArrayGroupList, NutrientGroupType } from '../../../views/ingredient-editor/form-generator/form-types';
+import { createNutrientGroup } from '../../../views/ingredient-editor/form-generator/primatives/multi-value/nutrition/nutrient-group/nutrient-group';
 
 @Component({
   selector: 'lib-vitamins-and-minerals-list',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './vitamins-and-minerals-list.component.html',
   styleUrls: ['./vitamins-and-minerals-list.component.css'],
 })
-export class VitaminsAndMineralsListComponent {}
+export class VitaminsAndMineralsListComponent {
+
+  @Input() Control! : FormGroup< FormArrayGroupList< NutrientGroupType > >;
+
+
+
+  addItem() : void {
+
+    this.Control.controls.list.controls.push( createNutrientGroup() );
+
+  }
+
+
+
+  removeItem(index : number) : void {
+
+    this.Control.controls.list.removeAt( index );
+
+  }
+
+  
+
+}
