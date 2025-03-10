cea4af045de04cbc42d2203ae0db47ce2e14672d
marcus <optiqvision@gmail.com>
2025-03-10 14:17:05 -0500

    generate note list and note components

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/note-list/note-list.component.ts
@@ -0,0 +1,30 @@
+/*
+
+NoteList
+
+PURPOSE :
+
+VIEWS :
+
+ELEMENTS :
+
+INTERACTIVITY :
+
+GETS DATA FROM :
+
+SENDS DATA TO :
+
+USER STORIES :
+
+*/
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-note-list',
+  imports: [CommonModule],
+  templateUrl: './note-list.component.html',
+  styleUrls: ['./note-list.component.css'],
+})
+export class NoteListComponent {}
---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/note-list/note/note.component.ts
@@ -0,0 +1,30 @@
+/*
+
+Note
+
+PURPOSE :
+
+VIEWS :
+
+ELEMENTS :
+
+INTERACTIVITY :
+
+GETS DATA FROM :
+
+SENDS DATA TO :
+
+USER STORIES :
+
+*/
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-note',
+  imports: [CommonModule],
+  templateUrl: './note.component.html',
+  styleUrls: ['./note.component.css'],
+})
+export class NoteComponent {}
