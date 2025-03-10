077304a61d8eef18f8b051175cdb52d7a3d1acd6
marcus <optiqvision@gmail.com>
2025-03-09 23:05:52 -0500

    generate weight component

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/nutrient-category/nutrient-category.component.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/nutrient-category/nutrient-category.component.ts
@@ -18,13 +18,29 @@ USER STORIES :
 
 */
 
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NutrientCategoryGroup } from '../../../views/ingredient-editor/form-generator/form-types';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TextInputComponent } from '@form-controls';
+import { ElementWeightComponent } from '../element-weight/element-weight.component';
 
 @Component({
   selector: 'lib-nutrient-category',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TextInputComponent,
+    ElementWeightComponent
+  ],
   templateUrl: './nutrient-category.component.html',
   styleUrls: ['./nutrient-category.component.css'],
 })
-export class NutrientCategoryComponent {}
+export class NutrientCategoryComponent {
+
+  @Input() Control! : FormGroup< NutrientCategoryGroup >;
+
+  NameToggle      = false;
+  NutrientsToggle = false;
+
+}
---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/weight/weight.component.ts
@@ -0,0 +1,30 @@
+/*
+
+Weight
+
+PURPOSE :
+
+VIEWS :
+
+ELEMENTS :
+
+INTERACTIVITY :
+
+GETS DATA FROM :
+
+SENDS DATA TO :
+
+USER STORIES :
+
+*/
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-weight',
+  imports: [CommonModule],
+  templateUrl: './weight.component.html',
+  styleUrls: ['./weight.component.css'],
+})
+export class WeightComponent {}
