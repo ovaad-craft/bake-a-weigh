1863718820b23db26369cf95d9a164856e5e71bb
marcus <optiqvision@gmail.com>
2025-03-09 21:31:56 -0500

    generate nutrient-category component

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/nutrient-category/nutrient-category.component.ts
@@ -0,0 +1,30 @@
+/*
+
+NutrientCategory
+
+PURPOSE :
+
+VIEWS :
+
+ELEMENTS :
+
+INTERACTIVITY :
+
+GETS DATA FROM :
+
+SENDS DATA TO :
+
+USER STORIES :
+
+*/
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-nutrient-category',
+  imports: [CommonModule],
+  templateUrl: './nutrient-category.component.html',
+  styleUrls: ['./nutrient-category.component.css'],
+})
+export class NutrientCategoryComponent {}
