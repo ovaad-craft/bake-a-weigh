9769745973fce2113eb8e48df73939223ef1f3d2
marcus <optiqvision@gmail.com>
2025-03-10 15:18:09 -0500

    configure and markup note component

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/components/note-list/note-list.component.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/note-list/note-list.component.ts
@@ -18,13 +18,40 @@ USER STORIES :
 
 */
 
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NoteComponent } from './note/note.component';
+import { FormArrayGroupList, IngredientNoteFormGroup } from '../../views/ingredient-editor/form-generator/form-types';
+import { createIngredientNoteGroup } from '../../views/ingredient-editor/form-generator/primatives/multi-value/ingredient-note-group/ingredient-note-group';
 
 @Component({
   selector: 'lib-note-list',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, NoteComponent],
   templateUrl: './note-list.component.html',
   styleUrls: ['./note-list.component.css'],
 })
-export class NoteListComponent {}
+export class NoteListComponent {
+
+  @Input() Control! : FormGroup< FormArrayGroupList< IngredientNoteFormGroup > >;
+
+
+
+  addNote() : void {
+
+    this.Control.controls.list.push( createIngredientNoteGroup() );
+
+  }
+
+
+
+  deleteNote( index : number ) : void {
+
+    this.Control.controls.list.removeAt( index );
+    
+  }
+
+
+
+
+}
