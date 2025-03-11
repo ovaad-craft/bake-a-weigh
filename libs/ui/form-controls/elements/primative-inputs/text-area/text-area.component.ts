3a80efb08efee09319208a49d4cf249fcbe19d7e
marcus <optiqvision@gmail.com>
2025-03-10 22:43:09 -0500

    generate text area component

---
+++ libs/ui/form-controls/elements/primative-inputs/text-area/text-area.component.ts
@@ -0,0 +1,30 @@
+/*
+
+TextArea
+
+PURPOSE :
+
+VIEWS :
+
+ELEMENTS :
+
+INTERACTIVITY :
+
+GETS DATA FROM :
+
+SENDS DATA TO :
+
+USER STORIES :
+
+*/
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-text-area',
+  imports: [CommonModule],
+  templateUrl: './text-area.component.html',
+  styleUrls: ['./text-area.component.css'],
+})
+export class TextAreaComponent {}
