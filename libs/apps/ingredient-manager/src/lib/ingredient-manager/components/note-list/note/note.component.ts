2015a8bf4062ef71f2de853c35fa547a1f4abddc
marcus <optiqvision@gmail.com>
2025-03-10 22:39:45 -0500

    configure and debug note list and note components

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/components/note-list/note/note.component.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/note-list/note/note.component.ts
@@ -18,13 +18,20 @@ USER STORIES :
 
 */
 
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TextInputComponent } from '@form-controls';
+import { IngredientNoteFormGroup } from '../../../views/ingredient-editor/form-generator/form-types';
 
 @Component({
   selector: 'lib-note',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, TextInputComponent],
   templateUrl: './note.component.html',
   styleUrls: ['./note.component.css'],
 })
-export class NoteComponent {}
+export class NoteComponent {
+
+  @Input() Control! : FormGroup< IngredientNoteFormGroup >;
+  
+}
