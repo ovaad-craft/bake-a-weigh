dc6bfc7173e19f026d6e2797bb94b2cd1ed94c1b
marcus <optiqvision@gmail.com>
2025-03-11 18:47:48 -0500

    add new options to schema

--- libs/ovaad-schematics/src/generators/ovaad-angular-component/schema.d.ts
+++ libs/ovaad-schematics/src/generators/ovaad-angular-component/schema.d.ts
@@ -1,10 +1,19 @@
-export type OvaadComponentType = 'standard' | 'custom form control'
+export type OvaadComponentType = 'standard' | 'custom form control';
+
+export type OvaadFormControlType = 'FormGroup' | 'FormControl';
+
+export interface CustomFormControlSpecs{
+  controlType    : string;
+  typeAnnotation : string;
+  listPropertyName?  : string;
+}
 
 export interface OvaadAngularComponentGeneratorSchema {
 
   name          : string;
   project       : string;
   location      : string;
-  componentType : OvaadComponentType;
+  componentType : string;
+  controlInfo?  : CustomFormControlSpecs;
   
 }
