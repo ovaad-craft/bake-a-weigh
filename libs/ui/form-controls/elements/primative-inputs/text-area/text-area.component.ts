19e01723b20623b6d44b2e1bac447e8e40b96682
marcus <optiqvision@gmail.com>
2025-03-10 22:54:26 -0500

    configure, markup and expose text area component

--- libs/ui/form-controls/elements/primative-inputs/text-area/text-area.component.ts
+++ libs/ui/form-controls/elements/primative-inputs/text-area/text-area.component.ts
@@ -18,13 +18,21 @@ USER STORIES :
 
 */
 
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-text-area',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './text-area.component.html',
   styleUrls: ['./text-area.component.css'],
 })
-export class TextAreaComponent {}
+export class TextAreaComponent {
+
+  @Input() Control! : FormControl< string | null >;
+  @Input() Label! : string;
+}
--- libs/ui/form-controls/index.ts
+++ libs/ui/form-controls/index.ts
@@ -1,3 +1,4 @@
 export * from './elements/primative-inputs/text-input/text-input.component';
 export * from './elements/primative-inputs/number-input/number-input.component';
 export * from './elements/primative-inputs/select-input/select-input.component';
+export * from './elements/primative-inputs/text-area/text-area.component';
