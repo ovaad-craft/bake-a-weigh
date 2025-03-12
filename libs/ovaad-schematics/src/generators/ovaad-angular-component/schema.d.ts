58aadafff016d998cb2d8bb79539122008775b64
marcus <optiqvision@gmail.com>
2025-03-11 19:59:22 -0500

    add option for global import path for types

--- libs/ovaad-schematics/src/generators/ovaad-angular-component/schema.d.ts
+++ libs/ovaad-schematics/src/generators/ovaad-angular-component/schema.d.ts
@@ -1,10 +1,12 @@
 export type OvaadComponentType = 'standard' | 'custom form control';
 
-export type OvaadFormControlType = 'FormGroup' | 'FormControl';
+export type OvaadFormControlType = 'FormGroup' | 'FormControl' | 'FormArrayGroup';
 
 export interface CustomFormControlSpecs{
   controlType    : string;
   typeAnnotation : string;
+  hasGlobalTypePath? : string;
+  globalTypePath? : string;
   listPropertyName?  : string;
 }
 
