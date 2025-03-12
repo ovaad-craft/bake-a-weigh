8c996d3c3a2a0b514aeaa1992986fd024b109db9
marcus <optiqvision@gmail.com>
2025-03-11 21:31:09 -0500

    configure cli options in generator

--- libs/ovaad-schematics/src/generators/ovaad-angular-component/ovaad-angular-component.ts
+++ libs/ovaad-schematics/src/generators/ovaad-angular-component/ovaad-angular-component.ts
@@ -6,7 +6,35 @@ import {
   getProjects
 } from '@nx/devkit';
 import * as path from 'path';
-import { OvaadAngularComponentGeneratorSchema as Schema } from './schema';
+import { CustomFormControlSpecs, OvaadAngularComponentGeneratorSchema as Schema } from './schema';
+
+
+
+
+
+function createImportScript( items : string[], location : string ) : string {
+  
+  return `import { ${ items.length > 1 ? items.join(', ') : items[0] } } from '${location}';`;
+
+}
+
+function createPropAnnotation( item : CustomFormControlSpecs) : string {
+
+  return `${ item.controlType }< ${ item.typeAnnotation } >`;
+
+}
+
+function createControlInput( item : CustomFormControlSpecs ) : string {
+
+  return `@Input() Control! : ${ createPropAnnotation( item ) };`;
+
+}
+
+export interface OvaadComponentOptions {
+  importList? : string[];
+  metaDataImports : string[];
+  propList : string[];
+}
 
 
 
@@ -17,6 +45,7 @@ import { OvaadAngularComponentGeneratorSchema as Schema } from './schema';
 export async function ovaadAngularComponentGenerator( tree: Tree, options: Schema ) {
 
   const componentNames          = names(options.name);
+  //const componentVars           = { componentType : options.componentType, ...componentNames};
   const projects                = getProjects(tree);
   const targetProject           = projects.get(options.project);
 
@@ -28,13 +57,55 @@ export async function ovaadAngularComponentGenerator( tree: Tree, options: Schem
   
   const targetPath = path.join( targetProject.root, options.location, componentNames.fileName );
 
+  const importList : string[] = [];
+
+  const metaDataImports : string[] = [];
+
+  const propList : string[] = [];
+
+
+  if ( options.componentType === 'custom form control' ) {
+
+    importList.push( createImportScript( [ 'Input' ], '@angular/core' ) );
+
+    if( options.controlInfo?.controlType === 'FormGroup' || options.controlInfo?.controlType === 'FormArrayGroup' ) {
+
+      importList.push( createImportScript( [ 'ReactiveFormsModule', 'FormGroup' ], '@angular/forms' ) );
+      metaDataImports.push( 'ReactiveFormsModule' );
+      
+    }
+    
+    if( options.controlInfo?.controlType === 'FormControl' ) {
+
+      importList.push( createImportScript( [ 'FormsModule', 'FormControl' ], '@angular/forms' ) );
+      metaDataImports.push( 'FormsModule' );
+
+    }
+
+    if( options.controlInfo?.hasGlobalTypePath === 'yes' && options.controlInfo.globalTypePath ) {
+
+      importList.push( createImportScript( [`${options.controlInfo.typeAnnotation}`], options.controlInfo.globalTypePath));
+
+    }
+
+    propList.push( createControlInput(options.controlInfo!) );
+
+  }
+
+  const componentOptions = {
+    importList : [...importList],
+    metaDataImports : [...metaDataImports],
+    propList : [...propList],
+    ...componentNames
+  }
+
   
   
   generateFiles(
     tree,
     path.join(__dirname, 'files'),
     targetPath,
-    { ...componentNames,  tmpl : '' }
+    { ...componentOptions,  tmpl : '' }
   );
   
   await formatFiles(tree);
