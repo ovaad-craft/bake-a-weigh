67effcb254702c96248975a79c1950cf1a190e1d
marcus <optiqvision@gmail.com>
2025-03-18 00:31:51 -0500

    write methods for creating import declarations.

--- libs/ovaad-schematics/src/generators/ovaad-angular-component/file-writer/file-writer.ts
+++ libs/ovaad-schematics/src/generators/ovaad-angular-component/file-writer/file-writer.ts
@@ -14,6 +14,9 @@ export class OvaadFileWriter {
     constructor( public project : ts.Node ) { this.Project = project; }
 
 
+    
+    
+    
     //  For finding nodes inside the project.
     scanNode< NodeType extends ts.Node, ItemType extends ts.Node >( source : NodeType, typePredicate : ( node : ts.Node ) => node is ItemType ) : ItemType[] {
 
@@ -29,6 +32,43 @@ export class OvaadFileWriter {
         scan( source );
 
         return nodeList;
+
+    }
+
+
+
+    //  Create new Import declaration item.
+    createNewImportDecalarationItem( item : string ) : ts.ImportSpecifier {
+
+        return ts.factory.createImportSpecifier(
+
+            false,
+            undefined,
+            ts.factory.createIdentifier( item )
+
+        );
+
+    }
+
+
+
+
+
+    //  Create new Import declaration
+    createNewImportDeclaration( item : string, location : string ) : ts.ImportDeclaration {
+
+        const newItem      : ts.ImportSpecifier = this.createNewImportDecalarationItem( item );
+        const itemList     : ts.NamedImports    = ts.factory.createNamedImports( [ newItem ] );
+        const importClause : ts.ImportClause    = ts.factory.createImportClause( false, undefined, itemList );
+
+        return ts.factory.createImportDeclaration(
+
+            undefined,
+            importClause,
+            ts.factory.createStringLiteral( location ),
+            undefined
+
+        );
 
     }
 
