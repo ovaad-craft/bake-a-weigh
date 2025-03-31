d28af6e974fc931dc3ac9ea08e8592872c7b98bb
marcus <optiqvision@gmail.com>
2025-03-31 13:50:13 -0500

    create props and import select component into milk specs component

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/components/ingredient-profile-manager/profiles/dairy-profile/components/milk-specs/milk-specs.component.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/ingredient-profile-manager/profiles/dairy-profile/components/milk-specs/milk-specs.component.ts
@@ -23,14 +23,24 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { MilkSpecsGroup } from '../../../../../../views/ingredient-editor/form-generator/form-types';
+import { MilkState, MilkType } from '@bake-a-weigh/site-types';
+import { SelectInputComponent } from '@form-controls';
 
 @Component({
   selector: 'lib-milk-specs',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SelectInputComponent
+
+  ],
   templateUrl: './milk-specs.component.html',
   styleUrls: ['./milk-specs.component.css'],
 })
 export class MilkSpecsComponent {
   @Input() Control!: FormGroup<MilkSpecsGroup>;
   @Input() Label!: string;
+  
+  TypeSelect  : MilkType = 'low fat';
+  StateSelect : MilkState = 'liquid';
 }
