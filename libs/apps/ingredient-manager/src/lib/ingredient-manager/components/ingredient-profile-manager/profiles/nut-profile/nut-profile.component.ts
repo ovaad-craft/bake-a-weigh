4fa5869573820a37d035c31246d652823a7183d3
marcus <optiqvision@gmail.com>
2025-04-02 15:46:45 -0500

    configure and markup nut profile

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/components/ingredient-profile-manager/profiles/nut-profile/nut-profile.component.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/ingredient-profile-manager/profiles/nut-profile/nut-profile.component.ts
@@ -23,10 +23,16 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { NutProfileGroup } from '../../../../views/ingredient-editor/form-generator/form-types';
+import { TextInputComponent, ToggleInputComponent } from '@form-controls';
 
 @Component({
   selector: 'lib-nut-profile',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TextInputComponent,
+    ToggleInputComponent
+  ],
   templateUrl: './nut-profile.component.html',
   styleUrls: ['./nut-profile.component.css'],
 })
