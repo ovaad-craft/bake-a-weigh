953728e592a33213b58cd2bb3a934c35c8907271
marcus <optiqvision@gmail.com>
2025-04-02 00:39:12 -0500

    configure and markup weight measurement group component

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/components/weight-measurement-group/weight-measurement-group.component.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/weight-measurement-group/weight-measurement-group.component.ts
@@ -22,15 +22,26 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule, FormGroup } from '@angular/forms';
-import { WeightMeasurementPrimative } from 'undefined';
+import { NumberInputComponent, SelectInputComponent } from '@form-controls';
+import { WeightType } from '@bake-a-weigh/site-types';
+import { WeightMeasurementPrimative } from '../../views/ingredient-editor/form-generator/form-types';
 
 @Component({
   selector: 'lib-weight-measurement-group',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    NumberInputComponent,
+    SelectInputComponent
+
+  ],
   templateUrl: './weight-measurement-group.component.html',
   styleUrls: ['./weight-measurement-group.component.css'],
 })
 export class WeightMeasurementGroupComponent {
-  @Input() Control!: FormGroup<WeightMeasurementPrimative>;
+  @Input() Control!: FormGroup< WeightMeasurementPrimative >;
   @Input() Label!: string;
+
+
+  WeightTypeSelection : WeightType = 'g';
 }
