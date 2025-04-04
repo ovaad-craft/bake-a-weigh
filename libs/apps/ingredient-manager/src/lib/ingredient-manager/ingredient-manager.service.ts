514ba73eeb5c5582365a7d4d1d43d2dc31dd1de9
marcus <optiqvision@gmail.com>
2025-04-04 13:11:22 -0500

    configure and markup seed profile component

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/ingredient-manager.service.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/ingredient-manager.service.ts
@@ -3715,7 +3715,7 @@ export class IngredientManagerService {
   getIngredientToEdit() : IngredientProfile {
     
     //return DemoIngredients[DemoIngredients.length - 2];
-    return DemoIngredients[2];
+    return DemoIngredients[1];
     
     //return this.IngredientToEdit
 
