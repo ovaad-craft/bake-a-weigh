2f7e5d93aa173b63b61a26a246b9809185e72d1f
marcus <optiqvision@gmail.com>
2025-04-04 12:26:39 -0500

    configure and markup salt profile component

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/components/ingredient-profile-manager/profiles/salt-profile/salt-profile.component.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/ingredient-profile-manager/profiles/salt-profile/salt-profile.component.ts
@@ -23,14 +23,26 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { SaltProfileGroup } from '../../../../views/ingredient-editor/form-generator/form-types';
+import { SelectInputComponent, ToggleInputComponent } from '@form-controls';
+import { NutrientTrackerComponent } from '../../../../views/ingredient-editor/form-generator/primatives/multi-value/nutrient-tracker/nutrient-tracker.component';
+import { SaltConsistency, SaltType } from '@bake-a-weigh/site-types';
 
 @Component({
   selector: 'lib-salt-profile',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SelectInputComponent,
+    ToggleInputComponent,
+    NutrientTrackerComponent    
+  ],
   templateUrl: './salt-profile.component.html',
   styleUrls: ['./salt-profile.component.css'],
 })
 export class SaltProfileComponent {
   @Input() Label = 'item label';
-  @Input() Control!: FormGroup<SaltProfileGroup>;
+  @Input() Control!: FormGroup< SaltProfileGroup >;
+
+  SaltTypeSelection        : SaltType        = 'other';
+  SaltConsistencySelection : SaltConsistency = 'other';
 }
--- libs/apps/ingredient-manager/src/lib/ingredient-manager/ingredient-manager.service.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/ingredient-manager.service.ts
@@ -3714,7 +3714,8 @@ export class IngredientManagerService {
 
   getIngredientToEdit() : IngredientProfile {
     
-    return DemoIngredients[DemoIngredients.length - 1];
+    //return DemoIngredients[DemoIngredients.length - 2];
+    return DemoIngredients[2];
     
     //return this.IngredientToEdit
 
