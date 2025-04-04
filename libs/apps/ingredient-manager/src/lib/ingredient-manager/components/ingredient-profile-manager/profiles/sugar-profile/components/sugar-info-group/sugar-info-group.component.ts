c54c4deb04b41261a2a27154956b032d3b67ea65
marcus <optiqvision@gmail.com>
2025-04-04 13:54:00 -0500

    configure and markup sugar info group component

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/components/ingredient-profile-manager/profiles/sugar-profile/components/sugar-info-group/sugar-info-group.component.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/ingredient-profile-manager/profiles/sugar-profile/components/sugar-info-group/sugar-info-group.component.ts
@@ -22,15 +22,24 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule, FormGroup } from '@angular/forms';
-import { CaneInfoGroup } from 'undefined';
+import { CaneInfoGroup } from '../../../../../../views/ingredient-editor/form-generator/form-types';
+import { SelectInputComponent } from '@form-controls';
+import { CaneType, SugarConsistencyType } from '@bake-a-weigh/site-types';
 
 @Component({
   selector: 'lib-sugar-info-group',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SelectInputComponent
+  ],
   templateUrl: './sugar-info-group.component.html',
   styleUrls: ['./sugar-info-group.component.css'],
 })
 export class SugarInfoGroupComponent {
-  @Input() Control!: FormGroup<CaneInfoGroup>;
-  @Input() Label!: string;
+  @Input() Control! : FormGroup< CaneInfoGroup >;
+  @Input() Label!   : string;
+
+  SugarConsistencyOptions : SugarConsistencyType = 'small granules';
+  SugarTypeOptions : CaneType = 'raw';
 }
