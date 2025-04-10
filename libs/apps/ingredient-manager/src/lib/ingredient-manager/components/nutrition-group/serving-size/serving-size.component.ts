02e80eef52b28e952db6f0c7cb62ad5630b010eb
marcus <optiqvision@gmail.com>
2025-04-10 16:19:54 -0500

    add select-buttons-input to serving size input

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/serving-size/serving-size.component.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/serving-size/serving-size.component.ts
@@ -22,7 +22,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ElementWeightGroupType } from '../../../views/ingredient-editor/form-generator/form-types';
-import { NumberInputComponent, SelectInputComponent } from '@form-controls';
+import { NumberInputComponent, SelectButtonsInputComponent, SelectInputComponent } from '@form-controls';
 import { WeightType } from '@bake-a-weigh/site-types';
 
 @Component({
@@ -32,6 +32,7 @@ import { WeightType } from '@bake-a-weigh/site-types';
     ReactiveFormsModule,
     FormsModule,
     NumberInputComponent,
+    SelectButtonsInputComponent,
     SelectInputComponent
   ],
   templateUrl: './serving-size.component.html',
