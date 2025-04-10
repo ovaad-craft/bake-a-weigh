256f8fd1ae09232a4d815653c728bac6f4083d2b
marcus <optiqvision@gmail.com>
2025-04-10 01:58:38 -0500

    add input for font size and functinoality for handling 0s

--- libs/ui/form-controls/elements/primative-inputs/number-input/number-input.component.ts
+++ libs/ui/form-controls/elements/primative-inputs/number-input/number-input.component.ts
@@ -20,17 +20,38 @@ USER STORIES :
 
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-number-input',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './number-input.component.html',
   styleUrls: ['./number-input.component.css'],
 })
 export class NumberInputComponent {
 
-  @Input()Control! : FormControl< number | null >;
-  @Input()Label?   : string;
+  @Input()Control!  : FormControl< number | null >;
+  @Input()Label?    : string;
+  @Input()FontSize! : string;
   @Input()InputSize = 4;
+
+
+
+  handleZero() : void {
+
+    const value : string[] = this.Control.value!.toString().split('');
+
+    if( value.length > 1 && value[ 0 ] === '0' && value[ 1 ] !== '.' ){
+      
+      value.shift();
+
+      this.Control.setValue( Number( value.join('') ) );
+
+    }
+
+  }
+
+
+
+  
 }
