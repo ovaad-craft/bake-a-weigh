af4e31e14a1359ac5b5aa723cbbde75605c0f98d
marcus <optiqvision@gmail.com>
2025-04-13 04:02:11 -0500

    create functionality to toggle fat types

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/total-fat/total-fat.component.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/total-fat/total-fat.component.ts
@@ -22,7 +22,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule, FormGroup } from '@angular/forms';
-import { NutrientCategoryGroup } from '../../../views/ingredient-editor/form-generator/form-types';
+import { NutrientCategoryGroup, TotalFatCategoryGroup } from '../../../views/ingredient-editor/form-generator/form-types';
 import { WeightComponent } from '../weight/weight.component';
 import { ElementWeightType } from '@bake-a-weigh/site-types';
 import { NumberInputComponent } from '@form-controls';
@@ -40,30 +40,110 @@ import { NumberInputComponent } from '@form-controls';
 })
 export class TotalFatComponent {
 
-  @Input() Control! : FormGroup< NutrientCategoryGroup >;
+  @Input() Control! : FormGroup< TotalFatCategoryGroup >;
   @Input() Label!   : string;
 
 
 
   ControlToggle = false;
-
   PreviousTotalAmount! : ElementWeightType;
+
+  SaturatedFatToggle = false;
+  PreviousSaturatedFatAmount! : ElementWeightType;
+
+  TransFatToggle = false;
+  PreviousTransFatsAmount! : ElementWeightType;
+
+  MonounsaturatedFatToggle = false;
+  PreviousMonounsaturatedFatAmount! : ElementWeightType;
+
+  PolyunsaturatedFatToggle = false;
+  PreviousPolyunsaturatedFatAmount! : ElementWeightType
 
 
 
   toggleControlOn() : void {
   
-      this.PreviousTotalAmount = this.Control.value.totalAmount as ElementWeightType;
-      this.ControlToggle = true;
+    this.PreviousSaturatedFatAmount = this.Control.value.totalAmount as ElementWeightType;
+    this.ControlToggle = true;
   
-    }
+  }
   
-    toggleControlOff( update : boolean ) : void {
+  toggleControlOff( update : boolean ) : void {
   
-      if( !update ) { this.Control.controls.totalAmount.setValue( this.PreviousTotalAmount ); }
+    if( !update ) { this.Control.controls.totalAmount.setValue( this.PreviousSaturatedFatAmount ); }
   
-      this.ControlToggle = false;
-    }
+    this.ControlToggle = false;
 
+  }
+  
+  
+  
+  toggleSaturatedFatOn() : void {
+  
+    this.PreviousSaturatedFatAmount = this.Control.value.saturatedFat as ElementWeightType;
+    this.SaturatedFatToggle = true;
+  
+  }
+  
+  toggleSaturatedFatOff( update : boolean ) : void {
+  
+    if( !update ) { this.Control.controls.saturatedFat!.setValue( this.PreviousSaturatedFatAmount ); }
+  
+    this.SaturatedFatToggle = false;
+
+  }
+  
+  
+  
+  toggleTransFatOn() : void {
+  
+    this.PreviousTransFatsAmount = this.Control.value.transFat as ElementWeightType;
+    this.TransFatToggle = true;
+  
+  }
+  
+  toggleTransFatOff( update : boolean ) : void {
+  
+    if( !update ) { this.Control.controls.transFat!.setValue( this.PreviousTransFatsAmount ); }
+  
+    this.TransFatToggle = false;
+
+  }
+  
+  
+  
+  toggleMonounsaturatedFatOn() : void {
+  
+    this.PreviousMonounsaturatedFatAmount = this.Control.value.monounsaturatedFat as ElementWeightType;
+    this.MonounsaturatedFatToggle = true;
+  
+  }
+  
+  toggleMonounsaturatedFatOff( update : boolean ) : void {
+  
+    if( !update ) { this.Control.controls.monounsaturatedFat!.setValue( this.PreviousMonounsaturatedFatAmount ); }
+  
+    this.MonounsaturatedFatToggle = false;
+
+  }
+  
+  
+  
+  togglePolyunsaturatedFatOn() : void {
+  
+    this.PreviousTotalAmount = this.Control.value.monounsaturatedFat as ElementWeightType;
+    this.MonounsaturatedFatToggle = true;
+  
+  }
+  
+  togglePolyunsaturatedFatOff( update : boolean ) : void {
+  
+    if( !update ) { this.Control.controls.monounsaturatedFat!.setValue( this.PreviousTotalAmount ); }
+  
+    this.MonounsaturatedFatToggle = false;
+
+  }
+  
 
 }
