eef6279914426a6c18f4792edfa0b2ad706012a4
marcus <optiqvision@gmail.com>
2025-04-14 15:25:54 -0500

    build createTotalCarbohydratesGroup method

---
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/views/ingredient-editor/form-generator/primatives/multi-value/nutrition/total-carbohydrates-group/total-carbohydrates-group.ts
@@ -0,0 +1,31 @@
+import { FormGroup } from "@angular/forms";
+import { TotalCarbohydratesCategory } from "@bake-a-weigh/site-types";
+import { TotalCarbohydratesGroup } from "../../../../form-types";
+import { createElementWeightGroup } from "../../element-weight-group/element-weight-group";
+import { createTotalSugarsGroup } from "./total-sugars-group/total-sugars-group";
+
+
+
+export function createTotalCarbohydratesGroup( defaultData? : TotalCarbohydratesCategory ) : FormGroup< TotalCarbohydratesGroup > {
+
+    const group : FormGroup< TotalCarbohydratesGroup > = new FormGroup< TotalCarbohydratesGroup >({
+
+        totalAmount : createElementWeightGroup( defaultData?.totalAmount ?? undefined )
+
+    });
+
+
+
+    if( defaultData?.dietaryFiber  ) {  group.addControl( 'dietaryFiber',  createElementWeightGroup( defaultData.dietaryFiber  ));  }
+
+    if( defaultData?.totalSugars   ) {  group.addControl( 'totalSugars',   createTotalSugarsGroup(   defaultData.totalSugars   ));  }
+
+    if( defaultData?.sugarAlcohols ) {  group.addControl( 'sugarAlcohols', createElementWeightGroup( defaultData.sugarAlcohols ));  }
+
+
+
+    return group;
+
+
+
+}
