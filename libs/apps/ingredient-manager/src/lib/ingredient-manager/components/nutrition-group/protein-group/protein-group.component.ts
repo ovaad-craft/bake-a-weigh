c08218604926fbebfd23c27056bc1a68479d9c96
marcus <optiqvision@gmail.com>
2025-04-15 23:48:07 -0500

    configure, markup and style protein component

--- libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/protein-group/protein-group.component.ts
+++ libs/apps/ingredient-manager/src/lib/ingredient-manager/components/nutrition-group/protein-group/protein-group.component.ts
@@ -21,13 +21,14 @@ USER STORIES :
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { ElementWeightGroupType } from '../../../views/ingredient-editor/form-generator/form-types';
 import { ElementWeightType } from '@bake-a-weigh/site-types';
+import { WeightComponent } from '../weight/weight.component';
 
 @Component({
   selector: 'lib-protein-group',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, WeightComponent],
   templateUrl: './protein-group.component.html',
   styleUrls: ['./protein-group.component.css'],
 })
@@ -37,12 +38,51 @@ export class ProteinGroupComponent implements OnInit {
   @Input() NewEntry = false;
 
   ControlToggle = false;
-  PreviousValue! : ElementWeightType;
+  PreviousAmount! : ElementWeightType;
 
 
 
   ngOnInit(): void {
     
     if( this.NewEntry ){ this.ControlToggle = true; }
+
   }
+
+
+
+  toggleControlOn() : void {
+    
+      this.PreviousAmount = this.Control.value as ElementWeightType;
+      this.ControlToggle = true;
+    
+    }
+    
+    
+    
+    toggleControlOff( update : boolean ) : void {
+    
+      if( !update && this.PreviousAmount !== undefined ) {
+  
+        if( this.Control.controls.percentage === undefined && this.PreviousAmount.percentage !== undefined) {
+              
+          this.Control.addControl( 'percentage', new FormControl< number | null >( null) );     
+                
+        }
+  
+        if( this.Control.controls.percentage !== undefined && this.PreviousAmount.percentage === undefined ){
+  
+          this.Control.removeControl( 'percentage' );
+          
+        }
+  
+  
+        this.Control.setValue( this.PreviousAmount );
+  
+      }
+    
+      this.ControlToggle = false;
+    
+    }
+
+
 }
